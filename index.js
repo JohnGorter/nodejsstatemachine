a270ea25114e0925bd0bfd61fafb8122858d963d
John Gorter <john.gorter@gmail.com>
2022-01-21 15:20:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,26 +7,7 @@ import { Actions, States, Machines } from './dmr.js'
 const machine = new Machines.DMR();
 const rl = new readline.Interface({input, output});
 
-machine.on("transition", States.IdleState, States.Preheating, () => writeLog("preheating"));
-machine.on("action", Actions.WarmUpAction, () => writeLog("starting to warm the shit up"));
-machine.on("state.enter", States.Preheating, () => { 
-    machine.timer = new Timer().start("HEATING UP: ", 100); 
-    setTimeout(() => { machine.timer.stop(); machine.do(new Actions.PreheatReadyAction())}, 5000)
-});
-machine.on("state.enter", States.CoolingDown, () => { 
-    machine.timer = new Timer().start("COOLING DOWN: ", 100); 
-    setTimeout(() => {  machine.timer.stop(); machine.do(new Actions.CooledDownAction())}, 5000)
-});
-machine.on("state.enter", States.PreheatReady, () => writeLog("preheating is done!"));
-machine.on("state.enter", States.Roasting, () => machine.timer = new Timer().start("ROASTING: ", 100));
-machine.on("state.leave", States.Roasting, () => machine.timer.stop());
-machine.on("state.leave", States.CoolingDown, () => writeLog("Cooled down"));
-
-// machine.on("state.enter", (s) => writeLog("state enter " + s));
-// const machine = new Machines.Pin();
-// console.log("1");
-// machine.on("state.enter", (s) => writeLog("state enter!!" + s));
-// console.log("2");
+// your code here
 
 
 startCommands();
